0e410a55633c7dd472f3972ba9b293ca568d7914
Jill Short <jillian.b.short@gmail.com>
2020-08-06 02:09:59 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,19 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def name
+    @name
+  end
+  
+  def deposit(balance)
+    balance = deposit + balance
+  end
+  
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,14 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :transfer_amount, :status
+  
+  def initialization(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+    sender.valid? && receiver.valid?
+  end
 end
