54a7d92a2c977a5413fec2470fdb12c80b41cf09
Jill Short <jillian.b.short@gmail.com>
2020-08-07 01:26:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,7 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     elsif @status == "complete"
       puts "Transaction was already excuted" 
+    else  
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
