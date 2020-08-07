34178ac8b32d4a79830204aba2b9a662e32147c0
Jill Short <jillian.b.short@gmail.com>
2020-08-07 01:30:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,16 +13,31 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance <= @amount
+    if @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
+
     elsif @status == "complete"
-      puts "Transaction was already excuted" 
-    else  
-      @sender.balance -= @amount
-      @receiver.balance += @amount
+      puts "Transaction was already excuted"
+    else
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
       @status = "complete"
-      end
+    end
   end
+  
+  
+  # def execute_transaction
+  #   if @sender.balance <= @amount
+  #     @status = "rejected"
+  #     return "Transaction rejected. Please check your account balance."
+  #   elsif @status == "complete"
+  #     puts "Transaction was already excuted" 
+  #   else  
+  #     @sender.balance -= @amount
+  #     @receiver.balance += @amount
+  #     @status = "complete"
+  #     end
+  # end
   
 end
