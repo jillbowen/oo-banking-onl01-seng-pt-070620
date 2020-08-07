6da4ea68e8c7eb2dd156594da82fb59ac5d607ea
Jill Short <jillian.b.short@gmail.com>
2020-08-07 01:33:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,13 +13,11 @@ class Transfer
   end
   
   def execute_transaction
-    if  
+    if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-      @amount = 0 
-      
-    else @sender.balance <= @amount
+    else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
